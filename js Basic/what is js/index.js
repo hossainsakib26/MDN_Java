facf09d1c2e3db50b6807c0166d265a53d135746
Sazzad Hossain <hossain.sakib26@hotmail.com>
2021-09-20 15:32:02 +0600

    how to add js in a page

--- js Basic/what is js/index.js
+++ js Basic/what is js/index.js
@@ -6,3 +6,20 @@ function updateName() {
 	let name = prompt('Please Enter A New Name');
 	para.textContent = 'Player 1: ' + name;
 }
+
+document.addEventListener("DOMContentLoaded", function() {
+
+
+	function createParagraph() {
+		let para = document.createElement('p');
+		para.textContent = 'You clicked the button!';
+		document.body.appendChild(para);
+	}
+
+	const buttons = document.querySelectorAll('button');
+
+	for (let i = 0; i < buttons.length; i++) {
+		buttons[i].addEventListener('click', createParagraph);
+	}
+
+})
