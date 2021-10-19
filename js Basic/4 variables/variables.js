42cace5f861fa8d2b3bd0bf7f5ae00cfdd1166b4
Sazzad Hossain <hossain.sakib26@hotmail.com>
2021-10-19 17:11:59 +0600

    complete variables

--- js Basic/4 variables/variables.js
+++ js Basic/4 variables/variables.js
@@ -7,3 +7,23 @@ button_A.onclick = function () {
     alert('Hello ' + name + ', nice to see you bro!');
     heading_A.textContent = 'Welcome to our era ' + name;
 }
+
+let myName = 'sazzad';
+let myAge;
+
+myAge = 29;
+
+let nameArray = ['Sazzad', 'Shaon', 'Mahedi'];
+let numberArray = [12, 12, 33];
+
+let animalObj = {name: '', handQty: 0, hasTail: false}
+
+animalObj.name = 'Dog';
+animalObj.handQty = 4;
+animalObj.hasTail = true;
+
+let myNumber = '500'; // oops, this is still a string
+typeof myNumber;
+myNumber = 500; // much better — now this is a number
+typeof myNumber;
+
